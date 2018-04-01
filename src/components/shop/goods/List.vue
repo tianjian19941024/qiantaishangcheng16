2c741616e84d3b9cd5f89f514572e73132083257
田晓剑 <1714425242@qq.com>
2018-04-01 23:19:02 +0800

    轮播图&右侧推荐列表

--- src/components/shop/goods/List.vue
+++ src/components/shop/goods/List.vue
@@ -5,9 +5,10 @@
            <div class="location">
                <span>当前位置：</span>
                <router-link to="">首页</router-link>
+               <router-link to="">购物商城</router-link>
            </div>
        </div>
-       <!-- 页面头部 - 分类列表与轮播图 -->
+       <!-- 页面头部 - 分类列表与轮播图 --> 
        <div class="section">
            <div class="wrapper">
                <div class="wrap-box">
@@ -17,98 +18,26 @@
                            <ul>
                            <!--此处声明下面可重复循环-->
                            
-                               <li>
+                               <li v-for ="item in top.catelist" :key="item.id">
                                    <h3>
                                        <i class="iconfont icon-arrow-right"></i>
-                                       <span>手机数码</span>
+                                       <span>{{ item.title }}</span>
                                        <p>
-                                       
-                                           手机通讯
-                                       
-                                           摄影摄像
-                                       
-                                           存储设备
-                                       
+                                         <span v-for="subitem in item.subcates" :key="subitem.id">{{ subitem.title }}&nbsp;</span>
                                        </p>
                                    </h3>
+                                   <!-- 鼠标填出层 -->
                                    <div class="item-box">
                                        <!--如有三级分类，此处可循环-->
                                        <dl>
-                                           <dt><a href="/goods/40.html">手机数码</a></dt>
+                                           <dt><a href="/goods/40.html">{{ item.title }}</a></dt>
                                            <dd>
-                                               
-                                               <a href="/goods/43.html">手机通讯</a>
-                                               
-                                               <a href="/goods/44.html">摄影摄像</a>
-                                               
-                                               <a href="/goods/45.html">存储设备</a>
-                                               
+                                             <router-link to="" v-for="subitem in item.subcates" :key="subitem.id">{{ subitem.title }}&nbsp;</router-link>                                                   
                                            </dd>
                                        </dl>
                                    </div>
                                </li>
-                           
-                               <li>
-                                   <h3>
-                                       <i class="iconfont icon-arrow-right"></i>
-                                       <span>电脑办公</span>
-                                       <p>
-                                       
-                                           电脑整机
-                                       
-                                           外设产品
-                                       
-                                           办公打印
-                                       
-                                       </p>
-                                   </h3>
-                                   <div class="item-box">
-                                       <!--如有三级分类，此处可循环-->
-                                       <dl>
-                                           <dt><a href="/goods/41.html">电脑办公</a></dt>
-                                           <dd>
-                                               
-                                               <a href="/goods/46.html">电脑整机</a>
-                                               
-                                               <a href="/goods/47.html">外设产品</a>
-                                               
-                                               <a href="/goods/48.html">办公打印</a>
-                                               
-                                           </dd>
-                                       </dl>
-                                   </div>
-                               </li>
-                           
-                               <li>
-                                   <h3>
-                                       <i class="iconfont icon-arrow-right"></i>
-                                       <span>影音娱乐</span>
-                                       <p>
-                                       
-                                           平板电视
-                                       
-                                           音响DVD
-                                       
-                                           影音配件
-                                       
-                                       </p>
-                                   </h3>
-                                   <div class="item-box">
-                                       <!--如有三级分类，此处可循环-->
-                                       <dl>
-                                           <dt><a href="/goods/42.html">影音娱乐</a></dt>
-                                           <dd>
-                                               
-                                               <a href="/goods/49.html">平板电视</a>
-                                               
-                                               <a href="/goods/50.html">音响DVD</a>
-                                               
-                                               <a href="/goods/51.html">影音配件</a>
-                                               
-                                           </dd>
-                                       </dl>
-                                   </div>
-                               </li>
+
                            
                            </ul>
                        </div>
@@ -117,27 +46,18 @@
                    
                    <!--幻灯片-->
                    <div class="left-705">
-                       <div class="banner-img">
-                           <div id="focus-box" class="focus-box">
-                               <ul class="slides">
-                                   <li class="" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 0; display: block; z-index: 1;">
-                                       <a href="/goods.html">
-                                           <img src="/templates/main/images/focus_1.png" draggable="false">
-                                       </a>
-                                   </li>
-                                   <li style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;" class="flex-active-slide">
-                                       <a href="/goods.html">
-                                           <img src="/templates/main/images/focus_2.png" draggable="false">
-                                       </a>
-                                   </li>
-                               </ul>
-                           <ol class="flex-control-nav flex-control-paging"><li><a class="">1</a></li><li><a class="flex-active">2</a></li></ol></div>
-                           
+                       <div class="banner-img">                         
+                        <el-carousel height="341px" style="overflow: hidden">
+                         <el-carousel-item v-for="item in top.sliderlist" :key="item.id">
+                            <img :src="item.img_url" :alt="item.title">
+                         </el-carousel-item>
+                        </el-carousel>        
                        </div>
                    </div>
                    <!--/幻灯片-->
                    
                    <!-- 侧边栏 - 推荐商品列表, 抽取为公共组件 -->
+                   <app-aside :list="top.toplist"></app-aside>
                    
                </div>
            </div>
@@ -147,8 +67,34 @@
 </template>
 
 <script>
+    import  AppAside from './subcom/CommonAside.vue';
+
     export default {
-        
+        components:{
+            AppAside
+        },
+        data(){
+            return{
+                top:{
+                   catelist: [],
+                   sliderlist: [],
+                   toplist: []  
+                }
+            }
+        },
+        methods:{
+            // 获取页面顶部数据
+            getTop(){
+                this.$http.get(this.$api.goodsTop).then(res =>{
+                    if(res.data.status ==0){
+                        this.top = res.data.message;
+                    }
+                })
+            }
+        },
+        created() {
+            this.getTop();
+        }
     }
 </script>
 
