7cddc74240321d0daadb2b4269aa66d56911b38c
田晓剑 <1714425242@qq.com>
2018-04-01 15:24:38 +0800

    商城公共头部底部&引入样式文件

--- src/components/shop/Shop.vue
+++ src/components/shop/Shop.vue
@@ -5,16 +5,17 @@
         <!-- 变化主体 -->
         <router-view></router-view>
         <!-- 公共底部 -->
-      
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
     import  appHeader from './subcom/Header.vue';
+    import appFooter from './subcom/Footer.vue';
     export default {        
             components:{
                 appHeader
-                
+                 appFooter
             }
     }
 </script>
